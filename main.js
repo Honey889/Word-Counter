5c69fb2b28079b119a6691912be1b42a591d1a6e
Hoor-Ul-Ain <156218686+Honey889@users.noreply.github.com>
2024-02-26 20:37:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const userInput = await inquirer.prompt([
+    {
+        type: "input",
+        name: "Paragraph",
+        message: "Enter your paragraph to count words"
+    }
+]);
+const paraInput = userInput.Paragraph;
+//  for counting letter in paragraph:
+const letterWithoutSpace = paraInput.replace(/\s/g, "");
+const letterCount = letterWithoutSpace.length;
+// console.log(letterCount);
+// for counting words:
+const wordsArray = paraInput.split(" ");
+const wordsCount = wordsArray.length;
+console.log(`Total letters in your para are ${letterCount} and total words are ${wordsCount}`);
---
+++ main.ts
@@ -0,0 +1,27 @@
+// import inquirer from "inquirer";
+
+// interface UserInput {
+//     Paragraph:string
+// }
+// const userInput : UserInput = await inquirer.prompt([
+//     {
+//         type: "input",
+//         name:"Paragraph",
+//         message:"Enter your paragraph to count words"
+//     }
+
+// ]);
+// const paraInput = userInput.Paragraph;
+
+// //  for counting letter in paragraph:
+
+// const letterWithoutSpace = paraInput.replace(/\s/g, "");
+// const letterCount = letterWithoutSpace.length;
+
+// // console.log(letterCount);
+
+
+// // for counting words:
+// const wordsArray = paraInput.split(" ")
+// const wordsCount = wordsArray.length
+// console.log(`Total letters in your para are ${letterCount} and total words are ${wordsCount}`);
